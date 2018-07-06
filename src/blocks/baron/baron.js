4c871eaa563bf345bb633fd4830a3f35dc85de32
Andrey Merkushov <mercmero@mail.ru>
2018-07-06 23:52:00 +0300

    pug_test

---
+++ src/blocks/baron/baron.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  baron({
+    root: '#baron-demo',
+    scroller: '.baron__scroller',
+    bar: '.baron__bar',
+    scrollingCls: 'baron--scrolling',
+    draggingCls: 'baron--dragging',
+    barOnCls: 'baron--scrollbar',
+  });
+
+});
